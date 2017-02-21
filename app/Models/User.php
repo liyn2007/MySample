d711fe8675f0b8b88b1a44751485da3587499359
Gavin Wang <liyn2007@qq.com>
2017-02-21 09:33:58 +0800

    by Gavin

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,7 +76,7 @@ class User extends Model implements AuthenticatableContract,
     {
         // return $this->statuses()->orderBy('created_at', 'desc');
         $user_ids = Auth::user()->followings->pluck('id')->toArray();
-        array_push($user_ids, Auth::user->id);
+        array_push($user_ids, Auth::user()->id);
 
         return Status::whereIn('user_id', $user_ids)
                         ->with('user')
